c7ae8f1d5d94385397bfc831a78f91bae35555a4
aaryxnblondead <aaryansingh2810@gmail.com>
2025-11-17 14:19:25 +0530

    reset

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['universe/native', 'universe/shared/typescript-analysis'],
-  rules: {
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/react-in-jsx-scope': 'off'
-  }
-};
--- src/types/navigation.ts
+++
@@ -1,5 +0,0 @@
-export type RootStackParamList = {
-  Menu: undefined;
-  Cart: undefined;
-  OrderSummary: undefined;
-};
